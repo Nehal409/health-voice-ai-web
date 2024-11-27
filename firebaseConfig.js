2b72f38f1433dbf2fe932a988cc4d835ac6879a5
zahidkhan-xen <zk8127194@gmail.com>
2024-11-27 11:39:58 +0500

    update firebase config variable

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDKOdE9BuMomBMVufB95ZxfQKQA2amCt9Y",
-  authDomain: "ehsaan-healthai.firebaseapp.com",
-  projectId: "ehsaan-healthai",
-  storageBucket: "ehsaan-healthai.firebasestorage.app",
-  messagingSenderId: "629773091315",
-  appId: "1:629773091315:web:3f017a44ac33a50e535286",
-  measurementId: "G-WCB82TT7EP"
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
